3e1f8117df53719cd018d431b3404030d86d2aac
olim007 <olimzhon01@mail.ru>
2025-07-25 12:02:35 +0500

    lecture13

--- pkg/main.go
+++ pkg/main.go
@@ -10,11 +10,28 @@ func main() {
 	svc := &wallet.Service{}
 	// wallet.RegisterAccount(svc, "+992345456345")
 	// svc.RegisterAccount("+992254365855")
-	svc.Deposit(1, 10)
+	// svc.Deposit(1, 10)
 	account, err := svc.RegisterAccount("+992987654321")
 	if err != nil {
 		fmt.Println("err")
 		return
 	}
-	fmt.Print(account)
+	fmt.Println(*account)
+	newAcc, err := svc.RegisterAccount("+992987654321")
+	if err != nil {
+		s := err.Error()
+
+		fmt.Println(err)
+		fmt.Println(s)
+		return
+	}
+	fmt.Print(*newAcc)
+
+	findAccount, err := svc.FindAccountById(12)
+	if err != nil {
+		fmt.Println(err)
+		return
+	} 
+	fmt.Println(findAccount)
+
 }
--- pkg/wallet/service.go
+++ pkg/wallet/service.go
@@ -1,12 +1,38 @@
 package wallet
 
-import "github.com/olim007/wallet/pkg/types"
+import (
+	"errors"
+
+	"github.com/google/uuid"
+	"github.com/olim007/wallet/pkg/types"
+)
 
 type Service struct {
 	nextAccountID int64
 	accounts  []*types.Account
 	payments []*types.Payment
 }
+
+// func (s *Service) Service(nextAccountID int64, accounts []*types.Account, payments []*types.Payment) {
+// 	s.accounts = accounts
+// 	s.payments = payments
+// 	s.nextAccountID = nextAccountID
+// }
+
+func (s *Service) FindAccountById(accountID int64) (*types.Account, error) {
+	account := &types.Account{}
+	for _, acc := range s.accounts {
+		if acc.ID == accountID {
+			account = acc
+			break
+		}
+	}
+	if account == nil {
+		return nil, ErrAccountNotFound
+	}
+	return account, nil
+}
+
 
 func RegisterAccount(service *Service, phone types.Phone) {
 	for _, account := range service.accounts {
@@ -25,7 +51,7 @@ func RegisterAccount(service *Service, phone types.Phone) {
 func (s *Service) RegisterAccount(phone types.Phone) (*types.Account, error) {
 	for _, account := range s.accounts {
 		if account.Phone == phone {
-			return nil, Error("Phone already registered")
+			return nil, ErrorExpression("Phone already registered")
 		}
 	}
 	s.nextAccountID++
@@ -40,7 +66,7 @@ func (s *Service) RegisterAccount(phone types.Phone) (*types.Account, error) {
 
 func (s *Service) Deposit(accountID int64, amount types.Money) error  {
 	if amount <= 0 {
-		return Error("amount must be greater than 0")
+		return ErrorExpression("amount must be greater than 0")
 	}
 
 	var account *types.Account 
@@ -52,11 +78,48 @@ func (s *Service) Deposit(accountID int64, amount types.Money) error  {
 	}
 
 	if account == nil {
-		return Error("account not found")
+		return ErrorExpression("account not found")
 	}
 
 	account.Balance += amount
 	return nil
+}
+
+var ErrAmountMustBePositive = errors.New("amount must be positive")
+var ErrAccountNotFound = errors.New("account not found")
+var ErrNotEnoughBalance = errors.New("not enough balance")
+
+func (s *Service) Pay(accountID int64, amount types.Money, category types.PaymentCategory) (*types.Payment, error) {
+	if amount <= 0 {
+		return nil, ErrAmountMustBePositive
+	}
+
+	var account *types.Account
+	for _, acc := range s.accounts {
+		if (*acc).ID == accountID {
+			account = acc
+			break
+		}
+	}
+	if account == nil {
+		return nil, ErrAccountNotFound
+	}
+
+	if (*account).Balance < amount {
+		return nil, ErrNotEnoughBalance
+	}
+
+	account.Balance -= amount
+	paymentID := uuid.New().String()
+	payment := &types.Payment{
+		ID: paymentID,
+		AccountID: accountID,
+		Amount: amount,
+		Category: category,
+		Status: types.PaymentStatusInProgress,
+	}
+	(*s).payments = append((*s).payments, payment)
+	return payment, nil
 }
 
 
@@ -66,7 +129,6 @@ type Messenger interface {
 }
 
 type Telegram struct {
-
 }
 
 func (t *Telegram) Send(message string) bool {
@@ -77,8 +139,8 @@ func (t *Telegram) Receive() (message string, ok bool) {
 	return "", true
 }
 
-type Error string
+type ErrorExpression string
 
-func (e Error) Error() string {
+func (e ErrorExpression) Error() string {
 	return string(e)
 }
